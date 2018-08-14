b6c807130ec96a758bb280dc7c1eddbdeb0b98f0
Loky <puiualexandru87@yahoo.com>
2018-08-14 18:03:21 +0200

    I refactored the code to use only Spring's application.properties file. closes #5

--- src/main/java/com/bishoptod3/diexercise/config/PropertyConfig.java
+++ src/main/java/com/bishoptod3/diexercise/config/PropertyConfig.java
@@ -2,28 +2,23 @@ package com.bishoptod3.diexercise.config;
 
 import com.bishoptod3.diexercise.examplebeans.FakeDataSource;
 import com.bishoptod3.diexercise.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 /**
  * Created by Loky on 13/08/2018.
  */
 @Configuration
 //@PropertySource( {"classpath:datasource.properties", "classpath:jms.properties"} )
-@PropertySources( {
-        @PropertySource( "classpath:datasource.properties" ),
-        @PropertySource( "classpath:jms.properties" )
-} )
+//@PropertySources( {
+//        @PropertySource( "classpath:datasource.properties" ),
+//        @PropertySource( "classpath:jms.properties" )
+//} )
 public class PropertyConfig {
 
-    @Autowired
-    Environment environment;
+//    @Autowired
+//    Environment environment;
 
     @Value( "${example.username}" )
     String username;
@@ -46,7 +41,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource getFakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser( environment.getProperty( "USERNAME" ) );
+        fakeDataSource.setUser( username );
         fakeDataSource.setPassword( password );
         fakeDataSource.setUrl( url );
 
@@ -62,8 +57,8 @@ public class PropertyConfig {
         return fakeJmsBroker;
     }
 
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        return new PropertySourcesPlaceholderConfigurer();
-    }
+//    @Bean
+//    public static PropertySourcesPlaceholderConfigurer properties() {
+//        return new PropertySourcesPlaceholderConfigurer();
+//    }
 }
