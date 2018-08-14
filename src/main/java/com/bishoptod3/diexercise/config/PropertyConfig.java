286b9a19c96e3f00187a3f27d35798a840b1c2f4
Loky <puiualexandru87@yahoo.com>
2018-08-14 16:48:48 +0200

    I set up a Spring Environment Property. closes #2

--- src/main/java/com/bishoptod3/diexercise/config/PropertyConfig.java
+++ src/main/java/com/bishoptod3/diexercise/config/PropertyConfig.java
@@ -1,11 +1,13 @@
 package com.bishoptod3.diexercise.config;
 
 import com.bishoptod3.diexercise.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 /**
  * Created by Loky on 13/08/2018.
@@ -13,6 +15,9 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 @Configuration
 @PropertySource( "classpath:datasource.properties" )
 public class PropertyConfig {
+
+    @Autowired
+    Environment environment;
 
     @Value( "${example.username}" )
     String username;
@@ -26,7 +31,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource getFakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser( username );
+        fakeDataSource.setUser( environment.getProperty( "USERNAME" ) );
         fakeDataSource.setPassword( password );
         fakeDataSource.setUrl( url );
 
